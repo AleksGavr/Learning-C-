5ad14cc5b97984e6d9b69a68b80af207cb6f6773
AlexeyN <aleksgavr@gmail.com>
2022-06-20 14:42:23 +1200

    Completed practical task for lesson 4Completed practical task for lesson 4

---
+++ Practicle/HW_4/Task 25/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход
+два числа (A и B) и возводит число A в натуральную
+степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Введите степень в которую надо возвести число: ");
+int power = int.Parse(Console.ReadLine());
+
+int NumPower(int power)
+{
+    int i = 1;
+    int result = 1;
+    while (i <= power)
+    {
+        result = result * num;
+        i++;
+    }
+    return result;
+}
+
+Console.Write($"{num}, {power} -> {NumPower(power)}");
---
+++ Practicle/HW_4/Task 29/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив
+из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+Console.Write("Введите длину массива: ");
+int length = int.Parse(Console.ReadLine());
+
+string UserArray = "";
+string Array(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        if (i == length - 1)
+        {
+            array[i] = new Random().Next(-100, 100);
+            UserArray = UserArray + array[i].ToString() + " -> [";
+            break;
+        }
+        else
+        {
+            array[i] = new Random().Next(-100, 100);
+            UserArray = UserArray + array[i].ToString() + ", ";
+        }
+    }
+    for (int i = 0; i < length; i++)
+    {
+        if (i == 0 && i == array.Length - 1)
+        {
+            UserArray = UserArray + " [" + array[i].ToString() + "]";
+            return UserArray;
+        }
+    
+        if (i == array.Length - 1)
+        {
+            UserArray = UserArray + array[i].ToString() + "]";
+            return UserArray;
+        }
+        else
+        {
+            UserArray = UserArray + array[i].ToString() + ", ";
+        }
+    }
+return UserArray;
+}
+
+Console.WriteLine(Array(length));
