f4e7d63c8daabd790d9e23089d192b44f82f056a
AlexeyN <aleksgavr@gmail.com>
2022-06-26 16:39:45 +1200

    Task 34 done

--- Practicle/HW_5/Task 34/Program.cs
+++ Practicle/HW_5/Task 34/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными
+// положительными трёхзначными числами. Напишите
+// программу, которая покажет количество чётных чисел в
+// массиве.
+// [345, 897, 568, 234] -> 2
+
+void Array(int[] arr)
+{
+    Console.Write("[");
+    int i = 0;
+    while (i < arr.Length + 1)
+    {
+        if (i == arr.Length -1)
+        {
+            arr[i] = new Random().Next(100, 1000);
+            Console.Write($"{arr[i]}]");
+            break;
+        }
+        else
+        {
+            arr[i] = new Random().Next(100, 1000);
+            Console.Write($"{arr[i]}, ");
+        }
+        i++;
+    }
+}
+Console.Write("Ввведите длину массива: ");
+int[] mass = new int[int.Parse(Console.ReadLine())];
+Array(mass);
+
+int count =0;
+for(int i =0; i<mass.Length;i++)
+{
+    if(mass[i]%2==0)
+    {
+        count+=1;
+    }
+}
+Console.Write($" -> {count}");
