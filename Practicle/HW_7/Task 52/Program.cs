b0332041deb71eba54bbf574acde6b2fee57eb44
AlexeyN <aleksgavr@gmail.com>
2022-07-02 19:22:30 +1200

    Task 7 done

--- Practicle/HW_7/Task 52/Program.cs
+++ Practicle/HW_7/Task 52/Program.cs
@@ -1,39 +1,45 @@
-﻿class Program
+﻿// Задача 52: Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом
+// столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого
+// столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine();
+Console.Write("Введите количество строк матрицы: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов матрицы: ");
+int column = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int[,] arr = new int[row, column];
+
+for (int i = 0; i < row; i++)
+    for (int j = 0; j < column; j++)
+        arr[i, j] = new Random().Next(1, 10);
+
+for (int i = 0; i < row; i++)
 {
-
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Введите размер матрицы:");
-        int N = int.Parse(Console.ReadLine());
-        int M = int.Parse(Console.ReadLine());
-        int[,] matrix = new int[N, M];
-        Random rand = new Random();
-        int[] summ = new int[M];
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < M; j++)
-            {
-                matrix[i, j] = rand.Next(0, 10); //рандомные значения для елементов матрицы
-
-            }
-        }
-        Console.WriteLine("Матрица");
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < M; j++)
-            {
-
-                Console.Write($"{matrix[i, j]} "); //вывод матрицы
-                summ[i] += matrix[j, i];      //подсчет суммы колонки
-            }
-            Console.WriteLine();
-        }
-        Console.WriteLine();
-        foreach (double elem in summ)
-        {
-
-            Console.WriteLine(elem / M); //вывод среднего для колонки
-        }
-
-    }
+    for (int j = 0; j < column; j++)
+        Console.Write(arr[i, j] + " ");
+    Console.WriteLine();
 }
+
+Console.WriteLine();
+Console.Write("Среднеее арифмитическое каждого столбца: ");
+for (int i = 0; i < column; i++)
+{
+    double sum = 0;
+    for (int j = 0; j < row; j++)
+    {
+        sum += arr[j, i];
+    }
+    double aver = sum/row;
+    double result = Math.Round(aver, 1);
+    Console.Write(result + "; ");
+}
+Console.WriteLine();
+Console.WriteLine();
