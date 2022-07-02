35447907da24b3dac75665ff544554aecbaaa5cd
AlexeyN <aleksgavr@gmail.com>
2022-07-02 13:25:35 +1200

    Task 7 doneTask 7 done

---
+++ Practicle/HW_7/Task 47/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int m = 3, n = 4;
+double[,] mass = new double[m, n];
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = Math.Round((new Random().Next(-10, 10) + new Random().NextDouble()), 1);
+    }
+}
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        Console.Write(mass[i, j] + " ");
+    }
+    Console.WriteLine();
+}
